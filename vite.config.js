d2d94771ca03ac01002c6c972826ed4d111fdfac
micymike <mosesmichael878@gmail.com>
2024-11-30 15:53:32 +0300

    port fixing

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
     proxy: {
       '/v1': {
         target: 'http://localhost:8000',
@@ -13,12 +15,25 @@ export default defineConfig({
       }
     }
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  },
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020'
     }
   },
   build: {
-    target: 'es2020'
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
